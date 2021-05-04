684c4b78a14b3b186b9fb89b91d8bef18a5d001f
dtavana <datavana@gmail.com>
2021-05-03 22:52:57 -0400

    Create course endpoint

--- src/backend/controllers/api.controller.js
+++ src/backend/controllers/api.controller.js
@@ -1,28 +1,34 @@
-/* eslint-disable import/prefer-default-export */
+const pulledCourses = [
+    {
+        id: 0,
+        name: 'Green Hill Golf Course',
+        address: '123 Green St, Worcester MA',
+        rating: '4.4',
+        cost: '$$'
+    },
+    {
+        id: 1,
+        name: 'Worcester Country Club',
+        address: '546 Main St, Worcester MA',
+        rating: '3.6',
+        cost: '$'
+    },
+    {
+        id: 2,
+        name: 'Shaker Hill Country Club',
+        address: '146 Shaker Rd, Harvard MA',
+        rating: '4.9',
+        cost: '$$$'
+    }
+];
+
+export const course = (req, res) => {
+    // Pull courses here
+    const { id: courseId } = req.query;
+    res.status(200).send({ data: pulledCourses[courseId] });
+};
+
 export const courses = (req, res) => {
     // Pull courses here
-    const pulledCourses = [
-        {
-            id: 0,
-            name: 'Green Hill Golf Course',
-            address: '123 Green St, Worcester MA',
-            rating: '4.4',
-            cost: '$$'
-        },
-        {
-            id: 1,
-            name: 'Worcester Country Club',
-            address: '546 Main St, Worcester MA',
-            rating: '3.6',
-            cost: '$'
-        },
-        {
-            id: 2,
-            name: 'Shaker Hill Country Club',
-            address: '146 Shaker Rd, Harvard MA',
-            rating: '4.9',
-            cost: '$$$'
-        }
-    ];
     res.status(200).send({ data: pulledCourses });
 };
--- src/backend/routes/api/index.js
+++ src/backend/routes/api/index.js
@@ -1,7 +1,8 @@
 import express from 'express';
-import { courses } from '../../controllers/api.controller';
+import { course, courses } from '../../controllers/api.controller';
 
 const router = express.Router();
+router.get('/course', course);
 router.get('/courses', courses);
 
 export default router;
